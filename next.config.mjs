07bd60b012863f6da049c08fefb972dec312aba7
Anastasiia <anastasiia_mahalias@epam.com>
2025-11-12 01:04:35 +0200

    Clean gh-pages branch and deploy static files

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  distDir: "build",
-  assetPrefix: "/6037_book_session/",
-};
-
-export default nextConfig;
--- src/helpers/scrollContainer.ts
+++
@@ -1,7 +0,0 @@
-export const scrollContainer = (direction: "left" | "right", ref: React.RefObject<HTMLDivElement>) => {
-  if (!ref.current) return;
-  ref.current.scrollBy({
-    left: direction === "left" ? -300 : 300,
-    behavior: "smooth",
-  });
-};
